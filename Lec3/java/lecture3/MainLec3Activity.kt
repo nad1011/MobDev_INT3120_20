8137a96d44949c69f945cde40fe664a6c7f26708
nad1011 <haidan10112003@gmail.com>
2023-09-07 21:25:21 +0700

    Update Lec3

---
+++ Lec3/java/lecture3/MainLec3Activity.kt
@@ -0,0 +1,12 @@
+package lecture3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobdev_homework.R
+
+class MainLec3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lec3)
+    }
+}
